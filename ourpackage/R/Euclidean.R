24fe17fb0b7d00dfb96bec7868d957293a448745
Eric Kamphuis <erika464@student.liu.se>
2019-09-16 21:47:26 +0200

    Now it works

--- ourpackage/R/Euclidean.R
+++ ourpackage/R/Euclidean.R
@@ -1,3 +1,12 @@
+#' Find the largest common divisor
+#' 
+#' @export euclidean
+#' @param a An integer
+#' @param b An integer
+#' @return The largest common divisor of \code{a} and \code{b}
+#' @examples 
+#' euclidean(123612, 13892347912)
+#' euclidean(100, 1000)
 euclidean <- function(a, b) {
   while (b != 0) {
     t <- b
@@ -6,12 +15,4 @@ euclidean <- function(a, b) {
   }
   return(a)
 }
-#' Find the largest common divisor
-#' 
-#' @param a An integer
-#' @param b An integer
-#' @return The largest common divisor of \code{a} and \code{b}
-#' @examples 
-#' euclidean(123612, 13892347912)
-#' euclidean(100, 1000)
 #' @references https://en.wikipedia.org/wiki/Euclidean_algorithm
