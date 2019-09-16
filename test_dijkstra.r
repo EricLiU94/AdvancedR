49fd58bdc549db909504754a86eb1032db301b00
JuiaLindohf <julia.lindohf@gmail.com>
2019-09-16 10:28:34 +0200

    A test

--- test_dijkstra.r
+++ test_dijkstra.r
@@ -1,3 +1,9 @@
-dijkstra <- function () {
+dijkstra <- function (wiki_graph, n) {
+  # attempt to create a matrix based upon v1 and v2 
+  nod<- wiki_graph[1]
+  lank<- wiki_graph[2]
+  vikt <- wiki_graph[3]
+  
+  # calculating the number of nodes 
   
 }
