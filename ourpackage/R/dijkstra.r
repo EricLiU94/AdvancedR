f91bbecb742c84f31975c9d38dac7c3d3e4d6ca0
Julia Lindohf <55234903+JuliaLindohf@users.noreply.github.com>
2019-09-19 00:47:02 +0200

    Update dijkstra.r

--- ourpackage/R/dijkstra.r
+++ ourpackage/R/dijkstra.r
@@ -5,6 +5,14 @@
 #' @param the initial node, from which node, the trip begins 
 #' @return, the expected shortest path, towards each and every node, through the entire network 
 #' @example 
+#' wiki_graph <-
+#' data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
+#'           v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
+#'           w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
+#' three rows, the first two rows descripts which connections are available
+#' the last row indicates the weight function
+#' initial_node: the starting node, from which the trip starts
+#' @references url { (https://en.wikipedia.org/wiki/Dijkstra%27s algorithm) } 
 
 dijkstra <- function (wiki_graph, initial_node)  {
 nod<- as.matrix(wiki_graph[1])
