61ca95d2e19c158a6dd57f21082dba06ed7cb8cd
Eric Kamphuis <erika464@student.liu.se>
2019-09-19 21:35:35 +0200

    Removed DS files and old data Changed data documentation

--- ourpackage/R/wiki_graph.R
+++ ourpackage/R/wiki_graph.R
@@ -1,17 +1,14 @@
-#' Wiki_graph is a data set, which contains three row data 
+#' Wiki_graph is a data set, which contains three columns of data 
 #' 
 #' @docType data
 #' 
-#' 
-#' @usage data(dijkstra)
-#' 
 #' @keywords datasets 
-#' @examples
-#' data(wiki_graph)
-#' wiki_graph is a data frame, which contains three row vectors
-#' v1 and v2, signifies the links
-#' w is a weight function
-#'
+#' @format A data frame with 3 variables:
+#' \describe{
+#'   \item{v1}{the first node in a link}
+#'   \item{v2}{the second node in a link}
+#'   \item{w}{the weight of an edge between the linked nodes}
+#' }
 #' 
 #' @references \url{https://sv.wikipedia.org/wiki/Dijkstras_algoritm}
 #'  
