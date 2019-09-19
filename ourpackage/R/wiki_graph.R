432eaacb17952c2c34e1685e47a1643c0e3af2f8
JuiaLindohf <julia.lindohf@gmail.com>
2019-09-19 21:09:23 +0200

    a new update

--- ourpackage/R/wiki_graph.R
+++ ourpackage/R/wiki_graph.R
@@ -1,1 +1,19 @@
+#' Wiki_graph is a data set, which contains three row data 
 #' 
+#' @docType data
+#' 
+#' 
+#' @usage data(dijkstra)
+#' 
+#' @keywords datasets 
+#' @examples
+#' data(wiki_graph)
+#' wiki_graph is a data frame, which contains three row vectors
+#' v1 and v2, signifies the links
+#' w is a weight function
+#'
+#' 
+#' @references \url{https://sv.wikipedia.org/wiki/Dijkstras_algoritm}
+#'  
+#' 
+"wiki_graph"
